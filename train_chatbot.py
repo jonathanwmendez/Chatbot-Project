6fe498d9827b464e5b46a520987d11829cf01ee6
Jonathan Mendez <jwmp001@gmail.com>
2025-11-10 16:26:02 -0500

    Imports the required libraries and loads the data from the intents file.

---
+++ train_chatbot.py
@@ -0,0 +1,11 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
